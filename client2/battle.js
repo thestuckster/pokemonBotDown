31ebd9a8d5451c42cad40d20fe88b2bf8a9f47f0
Spence Sellers <spencesellers@gmail.com>
2016-05-13 22:18:25 -0500

    Added setMoveData to battle.js

--- client2/battle.js
+++ client2/battle.js
@@ -7,10 +7,36 @@ class Battle extends EventEmitter {
         super();
         this.battleId = battleId;
         this.pclient = pokemonClient;
+        this.moves = null;
     }
 
     setOpponentName(name){
         this.opponentName = name;
+    }
+
+    // Sets the current move data.
+    // The moveData is the 'moves' field from the protocol request object.
+    setMoveData(moveData){
+        this.possibleMoves = {};
+        moveData.forEach(move => {
+            if (!move.disabled){
+                let moveEntry = {};
+                moveEntry.name = move.move;
+                moveEntry.pp = move.pp;
+                moveEntry.maxpp = move.maxpp;
+                moveEntry.target = move.target;
+
+                this.possibleMoves[move.id] = moveEntry;
+            }
+        });
+    }
+
+    getPossibleMoveIds(){
+        return Object.keys(this.possibleMoves);
+    }
+
+    start(){
+        this.emit('start');
     }
 
     win(){
